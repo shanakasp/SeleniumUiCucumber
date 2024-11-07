c7d64df9ea6e6c5f11e07e1ebb9cdc55109ae2b1
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-11-07 12:24:38 +0530

    first commit

---
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,21 @@
+package StepDefinitions;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/Features",
+        glue = {"StepDefinitions"},
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/HtmlReports",
+                "json:target/JSONReport/report.json",
+                "junit:target/XMLReports/report.xml"
+        },
+        tags = "@smoketest"
+)
+public class TestRunner {
+}
